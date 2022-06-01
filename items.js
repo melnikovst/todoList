2ce7e329ace617be1d5e9e8f9c6f2c062b7a792b
Stanislav Melnikov <melnikov.stan1slav@yandex.ru>
2022-06-01 10:11:06 +0300

    feat trash

--- items.js
+++ items.js
@@ -1,9 +1,9 @@
 const todos = [
     'Your task...',
+/*     'Your task...',
     'Your task...',
     'Your task...',
-    'Your task...',
-/*     'business 5',
+    'business 5',
     'business 6',
     'business 7',
     'business 8', */
